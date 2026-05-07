9894320b9dd143d5a36cc0c3eccfa2449345949e
Paras Bakshi <63099028+Paras-bakshi@users.noreply.github.com>
2026-05-08 02:32:01 +0530

    Add files via upload

---
+++ Project 7 Typescript/types.js
@@ -0,0 +1,5 @@
+function add_two_numbers(a, b) {
+    return a + b;
+}
+var x = add_two_numbers("2", 10);
+console.log(x);
---
+++ Project 7 Typescript/types.ts
@@ -0,0 +1,7 @@
+function add_two_numbers(a:number,b:number)
+{
+    return a+b;
+}
+
+let x=add_two_numbers(10,10);
+console.log(x);
